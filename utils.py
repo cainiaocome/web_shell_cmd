8027a49ec4e66a36d09d17386152dd708dc1b967
zhangjialin <cainiaocome@gmail.com>
2015-03-27 17:22:54 +0800

    remove pycache

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ headers = {'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q
             'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:24.0) Gecko/20140924 Firefox/24.0 Iceweasel/24.8.1'}
 
 #a simple port probe implementation
-def check_port(ip, port)
+def check_port(ip, port):
     try:
         sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
         sock.settimeout(2)
